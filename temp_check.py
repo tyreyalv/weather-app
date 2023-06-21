8f9b4413014944ed93e19c7dde8f604765ec5b9d
tyreyalv <email@tyreyalv.com>
2023-06-21 11:36:00 -0600

    redis db to track state of weather for single notifications

--- temp_check.py
+++ temp_check.py
@@ -3,16 +3,22 @@ import time
 import urllib.parse
 import os
 from dotenv import load_dotenv
+import redis
 
 load_dotenv()
 
 OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
 GOOGLE_CHAT_WEBHOOK = os.environ.get('GOOGLE_CHAT_WEBHOOK')
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 
 TEMP_THRESHOLD = 75
 CITY_NAME = "Fort Collins"
 STATE_CODE = "CO"
 COUNTRY_CODE = "US"
+STATE_KEY = 'state'
+
+redis_db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 def get_city_coordinates():
     city_name_encoded = urllib.parse.quote(CITY_NAME)
@@ -38,10 +44,17 @@ def main():
     lat, lon = get_city_coordinates()
     temp = get_current_temperature(lat, lon)
 
-    if temp > TEMP_THRESHOLD:
+    # Read the state from Redis
+    state = redis_db.get(STATE_KEY)
+    if state is not None:
+        state = state.decode()
+
+    if temp > TEMP_THRESHOLD and state != 'above':
         send_to_google_chat(f"Temperature in Fort Collins, CO is now over {TEMP_THRESHOLD} degrees. Current temperature: {temp} degrees.")
-    elif temp <= TEMP_THRESHOLD:
+        redis_db.set(STATE_KEY, 'above')
+    elif temp <= TEMP_THRESHOLD and state != 'below':
         send_to_google_chat(f"Temperature in Fort Collins, CO is now back under {TEMP_THRESHOLD} degrees. Current temperature: {temp} degrees.")
+        redis_db.set(STATE_KEY, 'below')
 
 if __name__ == "__main__":
     main()
