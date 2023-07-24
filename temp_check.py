c02b6c678a08e35f0e0ccdf193148e3738318e7e
tyreyalv <email@tyreyalv.com>
2023-07-24 09:56:44 -0600

    dev workflow yaml and changes to python for logging

--- temp_check.py
+++ temp_check.py
@@ -1,14 +1,16 @@
 import requests
-import time
-import urllib.parse
 import os
+import logging
 from dotenv import load_dotenv
 import redis
 
 load_dotenv()
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
-DISCORD_WEBHOOK = os.environ.get('DISCORD_WEBHOOK')  # renamed variable
+DISCORD_WEBHOOK = os.environ.get('DISCORD_WEBHOOK')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 
@@ -21,21 +23,37 @@ LONGITUDE = -105.066320
 
 redis_db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
-
 def get_current_temperature():
-    weather_api_endpoint = f"https://api.openweathermap.org/data/3.0/onecall?lat={LATITUDE}&lon={LONGITUDE}&appid={OPENWEATHERMAP_API_KEY}&units=imperial"
-    response = requests.get(weather_api_endpoint)
-    data = response.json()
-    return data['current']['temp']
+    try:
+        weather_api_endpoint = f"https://api.openweathermap.org/data/3.0/onecall?lat={LATITUDE}&lon={LONGITUDE}&appid={OPENWEATHERMAP_API_KEY}&units=imperial"
+        response = requests.get(weather_api_endpoint)
+        response.raise_for_status()
+        data = response.json()
+        return data['current']['temp']
+    except Exception as e:
+        logging.error(f"Failed to get temperature: {e}")
+        return None
 
-def send_to_discord(message):  # renamed function
-    data = {
-        "content": message  # changed key to 'content'
-    }
-    requests.post(DISCORD_WEBHOOK, json=data)  # changed variable to DISCORD_WEBHOOK
+def send_to_discord(message):  
+    try:
+        data = {
+            "content": message  
+        }
+        response = requests.post(DISCORD_WEBHOOK, json=data)
+        response.raise_for_status()
+    except Exception as e:
+        logging.error(f"Failed to send Discord notification: {e}")
 
 def main():
+    logging.info("Script started.")
+    
     temp = get_current_temperature()
+
+    if temp is None:
+        logging.error("Could not retrieve temperature. Exiting script.")
+        return
+
+    logging.info(f"Current temperature: {temp} degrees.")
 
     # Read the state from Redis
     state_high = redis_db.get(STATE_KEY_HIGH)
@@ -47,6 +65,7 @@ def main():
         state_low = state_low.decode()
 
     if temp > TEMP_THRESHOLD_HIGH and state_high != 'above':
+        logging.info(f"Temperature is now over {TEMP_THRESHOLD_HIGH} degrees.")
         send_to_discord(f"Temperature in Fort Collins, CO is now over {TEMP_THRESHOLD_HIGH} degrees. Current temperature: {temp} degrees.")
         redis_db.set(STATE_KEY_HIGH, 'above')
     elif temp <= TEMP_THRESHOLD_HIGH and state_high != 'below':
@@ -55,8 +74,11 @@ def main():
     if temp > TEMP_THRESHOLD_LOW and state_low != 'above':
         redis_db.set(STATE_KEY_LOW, 'above')
     elif temp <= TEMP_THRESHOLD_LOW and state_low != 'below':
+        logging.info(f"Temperature is now back under {TEMP_THRESHOLD_LOW} degrees.")
         send_to_discord(f"Temperature in Fort Collins, CO is now back under {TEMP_THRESHOLD_LOW} degrees. Current temperature: {temp} degrees.")
         redis_db.set(STATE_KEY_LOW, 'below')
+
+    logging.info("Script finished successfully.")
 
 if __name__ == "__main__":
     main()
