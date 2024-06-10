34e16ab40d95823629990d660246c6630a0c8cbb
Tyler Alvarado <talvarado@teren4d.com>
2024-06-10 18:09:57 +0000

    update to lat and long

--- temp_check.py
+++ temp_check.py
@@ -15,12 +15,12 @@ REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
-TEMP_THRESHOLD_HIGH = 78
-TEMP_THRESHOLD_LOW = 80
+TEMP_THRESHOLD_HIGH = 70
+TEMP_THRESHOLD_LOW = 75
 STATE_KEY_HIGH = 'state_high'
 STATE_KEY_LOW = 'state_low'
-LATITUDE = 40.589820
-LONGITUDE = -105.066320
+LATITUDE = 40.410160
+LONGITUDE = -105.104730
 
 redis_db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
@@ -69,7 +69,7 @@ def main():
 
     if temp > TEMP_THRESHOLD_HIGH and state_high != 'above':
         logging.info(f"Temperature is now over {TEMP_THRESHOLD_HIGH} degrees.")
-        send_to_discord(f"Temperature in Fort Collins, CO is now over {TEMP_THRESHOLD_HIGH} degrees. Current temperature: {temp} degrees.")
+        send_to_discord(f"Temperature in Loveland, CO is now over {TEMP_THRESHOLD_HIGH} degrees. Current temperature: {temp} degrees.")
         redis_db.set(STATE_KEY_HIGH, 'above')
     elif temp <= TEMP_THRESHOLD_HIGH and state_high != 'below':
         redis_db.set(STATE_KEY_HIGH, 'below')
@@ -78,7 +78,7 @@ def main():
         redis_db.set(STATE_KEY_LOW, 'above')
     elif temp <= TEMP_THRESHOLD_LOW and state_low != 'below':
         logging.info(f"Temperature is now back under {TEMP_THRESHOLD_LOW} degrees.")
-        send_to_discord(f"Temperature in Fort Collins, CO is now back under {TEMP_THRESHOLD_LOW} degrees. Current temperature: {temp} degrees.")
+        send_to_discord(f"Temperature in Loveland, CO is now back under {TEMP_THRESHOLD_LOW} degrees. Current temperature: {temp} degrees.")
         redis_db.set(STATE_KEY_LOW, 'below')
 
     logging.info("Script finished successfully.")
