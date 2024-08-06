2066839cdae9d0787718a32646df48055ac2e851
Tyler Alvarado <accounts@tyreyalv.com>
2024-08-06 08:34:13 -0700

    update to weatherservice class to use ray for api calls in parallel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import ray
 from dotenv import load_dotenv
 from src.config import Config
 from src.redis_service import RedisService
@@ -8,6 +9,7 @@ from src.notifier import Notifier
 from src.window_controller import WindowController
 from src.water import WateringScheduler
 
+# Load environment variables from .env file
 load_dotenv()
 
 class TemperatureMonitor:
@@ -35,21 +37,41 @@ class TemperatureMonitor:
 
         self.window_controller.check_and_update_window_state(temp, close_threshold, open_threshold)
 
+        # Call check_aqi_and_notify method
+        logging.info("Calling check_aqi_and_notify...")
+        self.window_controller.check_aqi_and_notify()
+
 
 if __name__ == "__main__":
+    # Initialize logging
     logging.basicConfig(level=logging.INFO)
+
+    # Initialize Ray
+    ray.init(address='ray://10.43.76.85:10001')
+
     try:
+        # Initialize RedisService
         redis_service = RedisService(Config.REDIS_HOST, Config.REDIS_PORT, Config.REDIS_PASSWORD)
     except ConnectionError as e:
         logging.error(f"Could not connect to Redis. Exiting script. Error: {e}")
         exit(1)
 
+    # Initialize WeatherService with redis_service
     weather_service = WeatherService(Config.OPENWEATHERMAP_API_KEY, redis_service)
+    
+    # Initialize Notifier
     notifier = Notifier(Config.DISCORD_WEBHOOK)
+    
+    # Initialize WindowController with redis_service and notifier
     window_controller = WindowController(redis_service, notifier)
     
+    # Initialize TemperatureMonitor with weather_service and window_controller
     temperature_monitor = TemperatureMonitor(weather_service, window_controller)
+    
+    # Initialize WateringScheduler with redis_service and notifier
     watering_scheduler = WateringScheduler(redis_service, notifier)
 
+    # Run TemperatureMonitor
     temperature_monitor.run()
-    #watering_scheduler.check_watering_time()
+    # Optionally run WateringScheduler
+    # watering_scheduler.check_watering_time()
